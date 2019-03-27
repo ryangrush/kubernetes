958086aab0440933a9e025a2e17ee1e17777b1d9
Ryan Grush <rgrush@gmail.com>
2019-03-27 16:08:43 -0500

    kubernetes yml files

--- kube/cleanup.sh
+++ kube/cleanup.sh
@@ -1,5 +1,9 @@
-kubectl delete secret sample-secret
-kubectl delete service postgres-service
-kubectl delete deployment postgres-deployment
-kubectl delete service rails-service
-kubectl delete deployment rails-deployment
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+kubectl delete -f $DIR/secrets/app-secret.yml
+# kubectl delete -f $DIR/volumes/postgres-volume.yml
+kubectl delete -f $DIR/services/postgres-service.yml
+kubectl delete -f $DIR/services/rails-service.yml
+kubectl delete -f $DIR/deployments/postgres-deployment.yml
+# kubectl delete -f $DIR/jobs/setup-job.yml
+kubectl delete -f $DIR/deployments/rails-deployment.yml
--- kube/run.sh
+++ kube/run.sh
@@ -1,7 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-kubectl create -f $DIR/secrets/sample-secret.yml
+kubectl create -f $DIR/secrets/app-secret.yml
+# kubectl create -f $DIR/volumes/postgres-volume.yml
 kubectl create -f $DIR/services/postgres-service.yml
-kubectl create -f $DIR/deployments/postgres-deployment.yml
 kubectl create -f $DIR/services/rails-service.yml
-kubectl create -f $DIR/deployments/rails-deployment.yml
+kubectl create -f $DIR/deployments/postgres-deployment.yml
+kubectl create -f $DIR/jobs/db-migrate.yml
+# kubectl create -f $DIR/deployments/rails-deployment.yml
